4f61d9c535bac626d8164522b94df22f8161e3eb
Alberto Abella <alberto.abella@fiware.org>
2025-04-07 00:46:40 +0200

    test of SQL export

---
+++ ServiceTechnician/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model ServiceTechnician of the subject dataModel.PredictiveMaintenance for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
+
+CREATE TABLE ServiceTechnician (calendar JSON, geographicalArea TEXT, hasSkill JSON, historicalService JSON, organization TEXT, referenceAddress TEXT, toolProvided JSON, type TEXT);
