10a3ff2e61dde6819bb0bf0f1a314adadfdea573
Alberto Abella <alberto.abella@fiware.org>
2025-04-07 00:46:39 +0200

    test of SQL export

---
+++ MaintenanceSkill/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model MaintenanceSkill of the subject dataModel.PredictiveMaintenance for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
+
+CREATE TABLE MaintenanceSkill (requiredTool JSON, skillDescription TEXT, skillDifficulty TEXT, skillName TEXT, type TEXT);
